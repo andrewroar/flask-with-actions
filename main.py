aff5d78ee072343dbf01b873d2a9316108e11518
andrewshout <andrewshout@gmail.com>
2025-07-19 09:48:32 +0100

    format

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    if request.method == 'POST':
+    if request.method == "POST":
         return "<p>Hello, World! You just made a POST request.</p>"
-    
+
     return "<p>Hello, World!</p>"
 
 
-@app.route("/test", methods=['GET' , 'POST'])
+@app.route("/test", methods=["GET", "POST"])
 def test():
     return "<p>Test</p>"
