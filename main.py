20ba2bb47d39a4d3b01483887fc08674e1445f0d
andrewshout <andrewshout@gmail.com>
2025-07-19 09:17:00 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        return "<p>Hello, World! You just made a POST request.</p>"
+    
+    return "<p>Hello, World!</p>"
